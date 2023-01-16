0a1c4c54a21593af0834ffcee6486c7263880313
kimicasamina <kimicasamina@gmail.com>
2023-01-16 17:15:12 +0800

    feat: added features that will display computers hand"

--- js/app.js
+++ js/app.js
@@ -1,8 +1,33 @@
 const choices = document.querySelectorAll('.choice')
 const hands = document.querySelectorAll('.player-hand .hand')
+const computerHands = document.querySelectorAll('.computer-hand .hand')
 let playersChoice;
+let computerChoice;
 
+function randomizeComputerChoice(){
+  randomNum = Math.floor(Math.random() * 3) + 1
+  console.log(randomNum)
+  if (randomNum === 1){
+    computerChoice = 'rock'
+  } else if (randomNum === 2){
+    computerChoice = 'paper'
+  } else if (randomNum === 3){
+    computerChoice = 'scissors'
+  }
 
+  selectComputerHand()
+  
+}
+
+function selectComputerHand(){
+  computerHands.forEach(hand => {
+    if (!hand.classList.contains(computerChoice)){
+      hand.style.display = 'none'
+    } else {
+      hand.style.display = 'flex'
+    }
+  })
+}
 
 choices.forEach(choice => {
   choice.addEventListener('click', (e) => {
@@ -12,13 +37,13 @@ choices.forEach(choice => {
       // console.log('player choose rock')
     } else if (e.target.value == 'paper'){
       playersChoice = 'paper'
-
       // console.log('player choose paper')
     } else if (e.target.value == 'scissors'){
       playersChoice = 'scissors'
   
       // console.log('player choose scissors')
     }
+    randomizeComputerChoice()
     selectHand()
   })
   
@@ -27,10 +52,8 @@ choices.forEach(choice => {
 function selectHand(){
   hands.forEach(hand => {
     if (!hand.classList.contains(playersChoice)){
-      console.log(playersChoice)
       hand.style.display = 'none'
     } else {
-      console.log(hand, 'not chosen')
       hand.style.display = 'flex'
   }
 })
