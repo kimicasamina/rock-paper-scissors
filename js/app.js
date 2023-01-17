41b0c5e846c2a5fce21c39cf60f2490968783463
kimicasamina <kimicasamina@gmail.com>
2023-01-17 14:02:44 +0800

    created getPlayersChoice, getComputersChoice, showHands, checkWinner function

--- js/app.js
+++ js/app.js
@@ -1,61 +1,145 @@
 const choices = document.querySelectorAll('.choice')
-const hands = document.querySelectorAll('.player-hand .hand')
+const playerHands = document.querySelectorAll('.player-hand .hand')
 const computerHands = document.querySelectorAll('.computer-hand .hand')
 let playersChoice;
-let computerChoice;
+let computerChoice
+let playersScore = 0
+let player
 
-function randomizeComputerChoice(){
-  randomNum = Math.floor(Math.random() * 3) + 1
-  console.log(randomNum)
-  if (randomNum === 1){
-    computerChoice = 'rock'
-  } else if (randomNum === 2){
-    computerChoice = 'paper'
-  } else if (randomNum === 3){
-    computerChoice = 'scissors'
-  }
 
-  selectComputerHand()
-  
-}
 
-function selectComputerHand(){
-  computerHands.forEach(hand => {
-    if (!hand.classList.contains(computerChoice)){
-      hand.style.display = 'none'
-    } else {
-      hand.style.display = 'flex'
-    }
-  })
-}
+// display hands 
 
 choices.forEach(choice => {
-  choice.addEventListener('click', (e) => {
-    e.preventDefault()
-    if (e.target.value == 'rock'){
-      playersChoice = 'rock'
-      // console.log('player choose rock')
-    } else if (e.target.value == 'paper'){
-      playersChoice = 'paper'
-      // console.log('player choose paper')
-    } else if (e.target.value == 'scissors'){
-      playersChoice = 'scissors'
-  
-      // console.log('player choose scissors')
-    }
-    randomizeComputerChoice()
-    selectHand()
+  choice.addEventListener('click', () => {
+    
+    // GET PLAYERS CHOICE
+    playersChoice = choice.value
+
+    // GET COMPUTERS CHOICE
+    getComputerChoice()
+
+    // CHECK WINNER AND SHOW HANDS AFTER 2 SECONDS
+    setTimeout(() => {
+      checkWinner(computerChoice, playersChoice)
+      showHands()
+    }, 1000);
+
   })
-  
 })
 
-function selectHand(){
-  hands.forEach(hand => {
-    if (!hand.classList.contains(playersChoice)){
-      hand.style.display = 'none'
-    } else {
-      hand.style.display = 'flex'
-  }
-})
+
+function showHands(){
+  // SHOW PLAYER HANDS
+  if (playersChoice == 'rock'){
+    playerHands.forEach(hand => {
+      if (!hand.classList.contains('rock')){
+        hand.style.display = 'none'
+      } else {
+        hand.style.display = 'flex'
+      }
+    })
+  } else if (playersChoice == 'paper'){
+    playerHands.forEach(hand => {
+      if (!hand.classList.contains('paper')){
+        hand.style.display = 'none'
+      } else {
+        hand.style.display = 'flex'
+      }
+    })
+  } else if (playersChoice == 'scissors'){
+    playerHands.forEach(hand => {
+      if (!hand.classList.contains('scissors')){
+        hand.style.display = 'none'
+      } else {
+        hand.style.display = 'flex'
+      }
+    })
+  } 
+
+  // SHOW COMPUTER HANDS 
+  if (computerChoice == 'rock'){
+    computerHands.forEach(hand => {
+      if (!hand.classList.contains('rock')){
+        hand.style.display = 'none'
+      } else {
+        hand.style.display = 'flex'
+      }
+    })
+  } else if (computerChoice == 'paper'){
+    computerHands.forEach(hand => {
+      if (!hand.classList.contains('paper')){
+        hand.style.display = 'none'
+      } else {
+        hand.style.display = 'flex'
+      }
+    })
+  } else if (computerChoice == 'scissors'){
+    computerHands.forEach(hand => {
+      if (!hand.classList.contains('scissors')){
+        hand.style.display = 'none'
+      } else {
+        hand.style.display = 'flex'
+      }
+    })
+  } 
 }
 
+function checkWinner(computerChoice, playersChoice){
+  if (playersChoice == 'rock'){
+    switch (computerChoice) {
+      case 'rock':
+        console.log("its a tie")
+        break;
+      case 'paper':
+        console.log("computer wins")
+        break;
+      case 'scissors':
+        console.log("player wins")
+        break;
+    }
+  } else if (playersChoice == 'paper'){
+    switch (computerChoice) {
+      case 'rock':
+        console.log("player wins")
+        break;
+      case 'paper':
+        console.log("its a tie")
+        break;
+      case 'scissors':
+        console.log("computer wins")
+        break;
+    }
+  } else if (playersChoice == 'scissors'){
+    switch (computerChoice) {
+      case 'rock':
+        console.log("computer wins")
+        break;
+      case 'paper':
+        console.log("players wins")
+        break;
+      case 'scissors':
+        console.log("its a tie")
+        break;
+    }
+  }
+}
+
+function getPlayersChoice(){
+  choices.forEach(choice => {
+    choice.addEventListener('click', () => {
+      playersChoice = choice.value
+    })
+  })
+}
+
+function getComputerChoice(){
+  const randNum = Math.floor(Math.random() * 3) + 1
+  if (randNum == 1){
+    computerChoice = 'rock'
+  } else if (randNum == 2){
+    computerChoice = 'paper'
+  } else if (randNum == 3){
+    computerChoice = 'scissors'
+  }
+}
